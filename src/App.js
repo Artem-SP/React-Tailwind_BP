42b73b38fd6ef670c464d8cf3cb4368e2e4b7212
Artem-SP <artem567gp@gmail.com>
2021-02-17 23:04:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-red-500"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
@@ -19,6 +19,8 @@ function App() {
         </a>
       </header>
     </div>
+    
+
   );
 }
 
